de17904267e16d924201ca44cc0d240f527e22ca
lucs2626bb <90346482+lucs2626bb@users.noreply.github.com>
2022-01-19 20:25:37 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,14 @@ function setup() {
   createCanvas(640, 360);
   // Make a Walker object
   w = new Walker();
+  
+  //Create a new slider:  https://p5js.org/reference/#/p5/createSlider
+  slider01 = createSlider(0, 255, 100);  //colour
+  slider01.position(10, 10);
+  slider01.style('width', '120px');
+  slider02 = createSlider(0, 255, 100);  //speed
+  slider02.position(10, 30);
+  slider02.style('width', '120px');
 }
 
 function draw() {
@@ -11,6 +19,7 @@ function draw() {
   // Update and display object
   w.update();
   w.display();
+  w.de
 }
 
 function Walker() {
@@ -20,15 +29,28 @@ function Walker() {
 
   this.update = function() {
     // Move Walker randomly
-    var vel = createVector(random(-5, 5), random(-5, 5));
+    let directionX = slider02.value() / 10;
+    let directionY = slider02.value() / 10;
+    var vel = createVector(random(-1 * directionX, directionX), random(-1* directionY, directionY));
+    
     this.pos.add(vel);
   }
 
   this.display = function() {
     // Draw Walker as circle
     //https://p5js.org/reference/#/p5/color
-    let c = color(255, 204, 0);
-    fill(c);
+    
+    let slideHue = slider01.value();
+    colorMode(HSB);
+    let slideColour = color(slideHue, 50, 250);
+    fill(slideColour);
+    
     ellipse(this.pos.x, this.pos.y, 48, 48);
   }
+  
+  this.debug = function() {
+     //debug
+    print(mouseX, " AND ", mouseY);
+  }
+  
 }
